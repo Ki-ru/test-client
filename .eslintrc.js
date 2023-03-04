1b80cff6291e4ccf0b9b5910913646930ce3d3a7
Leonid Perlin <blacksnow2K@outlook.com>
2023-03-04 18:32:12 +0300

    m

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,26 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    extends: [
+        '@react-native-community',
+        'airbnb-typescript',
+        'prettier',
+        'plugin:import/recommended'
+        // 'prettier/@typescript-eslint',
+        // 'prettier/react'
+    ],
+    parserOptions: {
+        project: ['./tsconfig.json']
+    },
+    settings: {
+        'import/ignore': [
+            'react-native',
+            'react-navigation',
+            'reduxjs/toolkit',
+            'expo-image-picker',
+            'react-redux'
+        ]
+    },
+    env: {
+        jest: true
+    }
 };
